d3b45439b2768ceedfcafd72284308a6950e79fa
turubut <76086301+turubut@users.noreply.github.com>
2025-01-04 08:00:28 +0300

    Update joined_orders_items.sql

--- joined_orders_items.sql
+++ joined_orders_items.sql
@@ -9,34 +9,34 @@
 
 WITH orders AS (
     SELECT
-    		id,
-		customer,
-		ordered_at,
-		store_id,
-		subtotal,
-		tax_paid,
-		order_total
+            id,
+        customer,
+        ordered_at,
+        store_id,
+        subtotal,
+        tax_paid,
+        order_total
     FROM {{ source('raw', 'raw_orders') }}
 ),
 
 items AS (
     SELECT 
-		id,
-		order_id,
-		sku
+        id,
+        order_id,
+        sku
     FROM {{ source('raw', 'raw_items') }}
 )
 
 SELECT
-    	o.id,
-	o.customer,
-	o.ordered_at,
-	o.store_id,
-	o.subtotal,
-	o.tax_paid,
-	o.order_total,
-	i.id as item_id,
-	i.sku  
+        o.id,
+    o.customer,
+    o.ordered_at,
+    o.store_id,
+    o.subtotal,
+    o.tax_paid,
+    o.order_total,
+    i.id as item_id,
+    i.sku  
 FROM orders o
 JOIN items i
     ON o.id = i.order_id
@@ -44,5 +44,8 @@ JOIN items i
 {% if is_incremental() %}
     -- Load records starting from the last loaded date (max ordered_at from previous loads)
     -- Only load data for the next 2 days
-    WHERE o.ordered_at >= (COALESCE((SELECT MAX(ordered_at) FROM {{ this }}), '2016-09-01'::timestamp) + INTERVAL '2 DAY')
+    WHERE o.ordered_at >= (COALESCE((SELECT MAX(ordered_at) - INTERVAL '2 DAYS' FROM {{ this }}), 
+                        '2016-09-01'::timestamp) - INTERVAL '2 DAY')
+    AND o.ordered_at < (COALESCE((SELECT MAX(ordered_at) + INTERVAL '2 DAYS' FROM {{ this }}), 
+                        '2016-09-01'::timestamp) + INTERVAL '2 DAY')
 {% endif %}
