9a031b99c9875cb7d4563bd221087f6a8914fc8b
turubut <76086301+turubut@users.noreply.github.com>
2025-01-04 06:57:57 +0300

    Update joined_orders_items.sql

--- joined_orders_items.sql
+++ joined_orders_items.sql
@@ -4,41 +4,30 @@
     materialized='incremental',
     unique_key='order_id',
     incremental_strategy='merge',
-    partition_by={'field': 'created_at', 'data_type': 'timestamp'}
+    partition_by={'field': 'ordered_at', 'data_type': 'timestamp'}
 ) }}
 
-WITH orders AS (
-    SELECT 
-        order_id,
-        customer_id,
-        order_date,
-        total_amount,
-        created_at
-    FROM {{ source('raw', 'raw_orders') }}
-),
-items AS (
-    SELECT 
-        item_id,
-        order_id,
-        product_id,
-        quantity,
-        price
-    FROM {{ source('raw', 'raw_items') }}
-)
+select * from
+(select 
+id,
+customer,
+,
+store_id,
+subtotal,
+tax_paid,
+order_total
+from {{ source('raw', 'raw_orders') }}) ro 
+left join 
+(select
+id,
+order_id,
+sku
+from {{ source('raw', 'raw_items') }} ri
+) as ri
+on ro.id  = ri.order_id
+order by ordered_at desc;
 
-SELECT
-    o.order_id,
-    o.customer_id,
-    o.order_date,
-    o.total_amount,
-    i.item_id,
-    i.product_id,
-    i.quantity,
-    i.price
-FROM orders o
-JOIN items i
-    ON o.order_id = i.order_id
 
 {% if is_incremental() %}
-    WHERE o.created_at > (SELECT MAX(created_at) FROM {{ this }})
+    WHERE ro.ordered_at > (SELECT MAX(ordered_at) FROM {{ this }})
 {% endif %}
